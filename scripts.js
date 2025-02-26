b9b28d849a8e82a0972c760ede7a0141bd5cb6c3
Sebastian Granobles Ardila <sebastiangranobles@hotmail.com>
2025-02-26 12:26:11 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ document.getElementById("form-hoja-vida").addEventListener("submit", function(ev
 
     // Obtener los valores del formulario
     const nombre = document.getElementById("nombre").value;
-    const  Historia = document.getElementById("Historia").value;
+    const Historia = document.getElementById("Historia").value;
     const estudios = document.getElementById("estudios").value;
     const referencias = document.getElementById("referencias").value;
     const habilidades = document.getElementById("habilidades").value;
